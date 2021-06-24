6007dc577145d41241dcfd933e0d9a4a7402027f
BlyNTC <isaacase@hotmail.com>
2021-06-24 18:59:36 -0300

    envia exercicio 3

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio3.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio3.js
@@ -1,1 +1,13 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0
+let qnt = numbers.length
+let media;
+
+for(index = 0; index < numbers.length; index += 1) {
+    soma = soma + numbers[index]
+}
+console.log(soma);
+console.log(media);
+
+media = soma / qnt
+console.log(media)
