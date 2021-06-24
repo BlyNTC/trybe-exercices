047de25aff11ebdbcc49b37d96ec7a116468ffe1
BlyNTC <isaacase@hotmail.com>
2021-06-24 20:30:46 -0300

    termina exercicio 6

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio6.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio6.js
@@ -1,1 +1,18 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let impar = []
+let impares = 0
+let index;
+
+for(index = 0; index < numbers.length; index += 1) {
+    let conta = numbers[index] % 2
+    if(conta > 0 ) {
+        impares += 1;
+        impar.push(numbers[index])
+    }
+}
+
+if(impares > 0) {
+    console.log(impares)
+} else {
+    console.log("nenhum valor ímpar encontrado");
+}
