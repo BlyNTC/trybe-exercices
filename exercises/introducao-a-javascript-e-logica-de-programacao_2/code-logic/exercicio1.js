5184a80b8fe73dca1eaf72c479c6cc5f98e18aab
BlyNTC <isaacase@hotmail.com>
2021-06-25 14:48:33 -0300

    termina primeiro exercicio tentando focar mais na  logica

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/code-logic/exercicio1.js
@@ -0,0 +1,8 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//percorra o array imprimindo
+//1- criar for que percorra os elementos do array
+//2- coloque o console.log no for para imprimir os elementos
+
+for(index = 0; index < numbers.length;index += 1) {
+    console.log(numbers[index])
+}
