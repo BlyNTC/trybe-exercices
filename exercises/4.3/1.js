86dec9b2cc2a558a23bbf864e260d6fc90a7f7ef
BlyNTC <isaacase@hotmail.com>
2021-06-26 20:31:48 -0300

    termina exercicio 1

--- exercises/4.3/1.js
+++ exercises/4.3/1.js
@@ -1,3 +1,10 @@
 //1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 ,
 //imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
+let n = 5
 
+for(let index = 1; index <= n; index += 1) {
+  let linhas = ''  
+    for(let linha = 1; linha <= n; linha += 1) {
+			linhas = linhas + '*'
+    }console.log(linhas)
+}
