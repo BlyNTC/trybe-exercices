229ec82ea4eab160e5db1d4366f6fa558a6ac55c
BlyNTC <isaacase@hotmail.com>
2021-06-26 21:28:43 -0300

    termina exercicio 2 e 3

--- exercises/4.3/2.js
+++ exercises/4.3/2.js
@@ -1,2 +1,14 @@
 //2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo
 //com 5 asteriscos de base. Por exemplo:
+let n = 5
+
+for(let index = 1; index <= n; index += 1) {
+  let linhas = ''  
+    for(let linha = 1; linha <= index; linha += 1) {
+        linhas += '*'
+    }
+    for(let spc = 1; spc <= n - index; spc += 1) {
+        linhas += ' '
+}
+    console.log(linhas)
+}
--- exercises/4.3/3.js
+++ exercises/4.3/3.js
@@ -1,1 +1,15 @@
 //3- Agora inverta o lado do triângulo. Por exemplo:
+
+let n = 5
+
+for(let index = 1; index <= n; index += 1) {
+  let linhas = ''  
+    for(let spc = 1; spc <= n - index; spc += 1) {
+    linhas += ' '
+    }
+    for(let linha = 1; linha <= index; linha += 1) {
+        linhas += '*'
+    }
+    
+    console.log(linhas)
+}
