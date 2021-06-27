0411c0de91f22f149d0ca88d935ae5f628e375e3
BlyNTC <isaacase@hotmail.com>
2021-06-26 22:23:31 -0300

    termina exercicio 4

--- exercises/4.3/4.js
+++ exercises/4.3/4.js
@@ -1,1 +1,18 @@
 //4- Depois, faça uma pirâmide com n asteriscos de base:
+let n = 5
+
+let n2 = 3
+
+for(let index = 0; index <= n; index += 2) {
+  let linhas = ''  
+    for(let spc = 1; spc <= n2 - index; spc += 2) {
+    linhas += ' '
+    }
+    for(let linha = 0; linha <= index; linha += 1) {
+        linhas += '*'
+    }
+    for(let spc2 = 1;spc2 <= n2 - index; spc2 += 2){
+        linhas += ' '
+    }
+    console.log(linhas)
+}
