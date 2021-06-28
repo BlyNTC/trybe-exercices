83a00afa6647cbe454adcdb12d1707f20b899fd1
BlyNTC <isaacase@hotmail.com>
2021-06-28 19:36:07 -0300

    arruma exercicio 2 by filipe

--- parte2/2.js
+++ parte2/2.js
@@ -1,5 +1,5 @@
 let array = [2, 3, 6, 7, 10, 1];
-let maiorValor = 0;
+let maiorValor = array[0];
 for (let index = 0; index < array.length; index++) {
     if(maiorValor < array[index]){
         maiorValor = index
