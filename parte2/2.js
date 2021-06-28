39c9c3f9f9852d778e9d18f4b33f53b3a6e3c160
BlyNTC <isaacase@hotmail.com>
2021-06-28 19:35:02 -0300

    exercicio 2 parte2

---
+++ parte2/2.js
@@ -0,0 +1,8 @@
+let array = [2, 3, 6, 7, 10, 1];
+let maiorValor = 0;
+for (let index = 0; index < array.length; index++) {
+    if(maiorValor < array[index]){
+        maiorValor = index
+    }
+}
+console.log(maiorValor);
