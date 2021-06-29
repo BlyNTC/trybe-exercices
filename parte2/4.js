bce1592ca98cc09cc00f538e254926a348f54e26
BlyNTC <isaacase@hotmail.com>
2021-06-28 21:25:53 -0300

    exercicio 4 parte 2 (sera q agora vai?)

--- parte2/4.js
+++ parte2/4.js
@@ -1,10 +1,13 @@
 // 4- Crie uma função que receba um array de nomes e 
 //retorne o nome com a maior quantidade de caracteres.
-    let array = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+let array = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']
+function nomeMaior(array){
     let maiorValor = array[0];
     for (let p of array) {
         if(p.length > maiorValor.length){
             maiorValor = p
         }
     }
-   console.log(maiorValor);
+    return console.log(maiorValor)
+}
+nomeMaior(array)
