e5dcf985deb5eef050511efafb9f9a765e66cbf3
BlyNTC <isaacase@hotmail.com>
2021-06-30 18:42:46 -0300

    cria pasta de exercicios 5.1

---
+++ parte2/bonus do inferno.js
@@ -0,0 +1,18 @@
+let object = {
+I: 1,
+IV: 4,
+V: 5,
+IX: 9,
+X: 10,
+XL: 40,
+L: 50,
+XC: 90,
+C: 100,
+CD: 400,
+D: 500,
+CM: 900,
+M: 1000
+}
+console.log(object.D*2);
+let array = [1, 0, 0, 0]
+array[0]
