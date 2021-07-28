4caa1a057f80dca5b3236b5092a8901aee9c9a7d
BlyNTC <isaacase@hotmail.com>
2021-07-28 14:47:46 -0300

    seja o que deus quiser

---
+++ 7.1/2part1.js
@@ -0,0 +1,14 @@
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// Seu código aqui.
+
+// console.log(oddsAndEvens.sort()); // será necessário alterar essa linha 😉
+
+// oddsAndEvens.sort(function(a, b) {
+//   return b - a;
+// });
+// console.log(oddsAndEvens);
+
+let array = [5,3,4,1]
+array.sort(function(a, b){return a-b});
+console.log(array);
