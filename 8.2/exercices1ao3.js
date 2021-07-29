d795e14fa6afb8573589656115da2abd06676aee
Isaac Matheus <85703133+BlyNTC@users.noreply.github.com>
2021-07-29 19:04:13 -0300

    arruma arquivo exercicio 1 ao 3

--- 8.2/exercices1ao3.js
+++ 8.2/exercices1ao3.js
@@ -86,3 +86,23 @@ function smallerName() {
   }
   console.log(smallerName());
   assert.strictEqual(smallerName(), 'Duna');
+  
+// 3 - Encontre o primeiro livro cujo nome possui 26 caracteres.
+
+const expectedResult = {
+  author: {
+    birthYear: 1948,
+    name: 'George R. R. Martin',
+  },
+  genre: 'Fantasia',
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  releaseYear: 1991,
+};
+
+function getNamedBook() {
+  // escreva seu código aqui
+  expectedResult
+}
+
+assert.deepStrictEqual(getNamedBook(), expectedResult);
