f8ff89d873ad7f27f0d167af5b53f42bb690ac14
Isaac Matheus <85703133+BlyNTC@users.noreply.github.com>
2021-07-29 19:06:28 -0300

    mescla exercices 1 ao 7

--- 8.2/exercices.js
+++ 8.2/exercices.js
@@ -106,3 +106,84 @@ function getNamedBook() {
 }
 
 assert.deepStrictEqual(getNamedBook(), expectedResult);
+
+// 4 - Ordene os livros por data de lançamento em ordem decrescente.
+const expectedResult = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: { name: 'George R. R. Martin', birthYear: 1948 },
+    releaseYear: 1991,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: { name: 'Stephen King', birthYear: 1947 },
+    releaseYear: 1986,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: { name: 'Frank Herbert', birthYear: 1920 },
+    releaseYear: 1965,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928,
+  },
+];
+
+function booksOrderedByReleaseYearDesc() {
+  // escreva aqui seu código
+}
+
+assert.deepStrictEqual(booksOrderedByReleaseYearDesc(), expectedResult);
+// 5 - Faça uma função que retorne true , se todas as pessoas autoras nasceram no século XX, ou false , caso contrário.
+
+const expectedResult = false;
+
+function everyoneWasBornOnSecXX() {
+  // escreva seu código aqui
+}
+
+assert.strictEqual(everyoneWasBornOnSecXX(), expectedResult);
+
+// 6 - Faça uma função que retorne true , se algum livro foi lançado na década de 80, e false , caso contrário.
+
+const expectedResult = true;
+
+function someBookWasReleaseOnThe80s() {
+  // escreva seu código aqui
+}
+
+assert.strictEqual(someBookWasReleaseOnThe80s(), expectedResult);
+
+// 7 - Faça uma função que retorne true , caso nenhum author tenha nascido no mesmo ano, e false , caso contrário.
+
+const expectedResult = false;
+
+function authorUnique() {
+  // escreva seu código aqui
+}
+
+assert.strictEqual(authorUnique(), expectedResult);
