d4030d0830096b8839eff35617acdf8ad3f02e28
Isaac Matheus <85703133+BlyNTC@users.noreply.github.com>
2021-07-29 18:14:10 -0300

    cria arquivos exercicios 8.2

---
+++ 8.2/exercices5ao7.js
@@ -0,0 +1,29 @@
+// 5 - Faça uma função que retorne true , se todas as pessoas autoras nasceram no século XX, ou false , caso contrário.
+
+const expectedResult = false;
+
+function everyoneWasBornOnSecXX() {
+  // escreva seu código aqui
+}
+
+assert.strictEqual(everyoneWasBornOnSecXX(), expectedResult);
+
+// 6 - Faça uma função que retorne true , se algum livro foi lançado na década de 80, e false , caso contrário.
+
+const expectedResult = true;
+
+function someBookWasReleaseOnThe80s() {
+  // escreva seu código aqui
+}
+
+assert.strictEqual(someBookWasReleaseOnThe80s(), expectedResult);
+
+// 7 - Faça uma função que retorne true , caso nenhum author tenha nascido no mesmo ano, e false , caso contrário.
+
+const expectedResult = false;
+
+function authorUnique() {
+  // escreva seu código aqui
+}
+
+assert.strictEqual(authorUnique(), expectedResult);
