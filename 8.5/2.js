6dfcae324f49f366d544a81e380d569a8dbb5941
Isaac Matheus <85703133+BlyNTC@users.noreply.github.com>
2021-08-03 18:33:50 -0300

    resolve 2

--- 8.5/2.js
+++ 8.5/2.js
@@ -4,6 +4,8 @@ const assert = require('assert');
 
 // escreva sum abaixo
 
+let sum = (...rest) => rest.reduce((acc,value) => acc + value, 0)
+
 assert.strictEqual(sum(), 0);
 assert.strictEqual(sum(1), 1);
 assert.strictEqual(sum(1, 2), 3);
