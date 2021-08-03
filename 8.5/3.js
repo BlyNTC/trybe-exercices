58587d84d56271b3af99ee83350aab8e87a31015
Isaac Matheus <85703133+BlyNTC@users.noreply.github.com>
2021-08-03 18:38:09 -0300

    resolve 3

--- 8.5/3.js
+++ 8.5/3.js
@@ -17,7 +17,12 @@ const gunnar = {
 };
 
 // complete a assinatura da função abaixo
-const personLikes = () => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
 
+const personLikes = (object) => {
+  let {name, age, likes, nationality} = object
+  return `${name} is ${age} years old and likes ${likes.join(', ')}.`
+};
+console.log(personLikes(alex));
+console.log(personLikes(gunnar));
 assert.strictEqual(personLikes(alex), 'Alex is 26 years old and likes fly fishing.');
 assert.strictEqual(personLikes(gunnar), 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.');
